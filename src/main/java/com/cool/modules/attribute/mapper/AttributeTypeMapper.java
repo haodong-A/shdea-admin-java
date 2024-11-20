bb095c7de4b42410bf9ffb485be97b632ac38b62
XiaoLiui <Sincnly@163.com>
2024-11-21 02:40:32 +0800

    init2

--- src/main/java/com/cool/modules/attribute/mapper/AttributeTypeMapper.java
+++
@@ -1,7 +0,0 @@
-package com.cool.modules.attribute.mapper;
-
-import com.cool.modules.attribute.entity.AttributeTypeEntity;
-import com.mybatisflex.core.BaseMapper;
-
-public interface AttributeTypeMapper extends BaseMapper<AttributeTypeEntity> {
-}
--- src/main/java/com/cool/modules/attribute/service/AttributeTypeService.java
+++
@@ -1,7 +0,0 @@
-package com.cool.modules.attribute.service;
-
-import com.cool.core.base.BaseService;
-import com.cool.modules.attribute.entity.AttributeTypeEntity;
-
-public interface AttributeTypeService extends BaseService<AttributeTypeEntity> {
-}
